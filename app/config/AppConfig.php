774bbb293aaff6fb17c018b28d2c870f947dd7df
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-16 21:43:06 -0300

    Commit base del proyecto

---
+++ app/config/AppConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+define("APP_URL","");
+define("APP_URI",$_SERVER["DOCUMENT_ROOT"]."/ldp_agenda/app/");
+
+CONST APP_TOKEN = "SECRET_KEY";
+
+define("APP_DEFAULT_ACTION", "index");
+
+
+?>
---
+++ app/config/DBConfig.php
@@ -0,0 +1,7 @@
+<?php
+
+CONST DB_DSN = "";
+CONST DB_USER = "root";
+CONST DB_PASS = "";
+
+?>
