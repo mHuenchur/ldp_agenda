9fad141cc3bd15743f76d89a59def3de262571a7
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-25 08:20:10 -0300

    Clase App run

---
+++ app/App.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app;
+
+use app\core\handler\base\Handler;
+use app\core\handler\ExceptionHandler;
+use app\core\handler\AuthenticationHandler;
+use app\core\handler\AuthorizationHandler;
+use app\core\handler\RoutingHandler;
+use app\libs\request\Request;
+use app\libs\response\Response;
+
+final class App{
+
+    private function __construct(){
+
+    }
+
+    public static function run(){
+
+        $exception = new ExceptionHandler();
+        $authentication = new AuthenticationHandler();
+        $authorization = new AuthorizationHandler();
+        $routing = new RoutingHandler();
+
+        $exception->setNext($authentication)->setNext($authorization)->setNext($routing);
+        //$exception->setNext($routing);
+        $request = new Request();
+        $response = new Response();
+
+
+        $exception->handle($request, $response);
+    }
+}
