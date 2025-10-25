6cbc4baed57cad7c484474b9ef1fa897e7847bfd
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-25 08:17:46 -0300

    Archivos DTO y su estructura base

---
+++ app/core/model/dto/CategoriaDTO.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\core\model\dto;
+
+use app\core\model\base\InterfaceDTO;
+
+final class CategoriaDTO  implements InterfaceDTO
+{
+    private $identificador;
+
+
+public function __construct($data = [])
+{
+
+}
+
+
+
+
+// ** METODOS **
+public function toArray(): array{
+    return [
+
+    ];
+}
+}
