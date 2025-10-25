197ed1df6beedd907a1255136bd13db41c5d4797
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-25 08:20:55 -0300

    Request y Response para trabajar entre capas

---
+++ app/libs/request/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\libs\request;
+
+final class Request{
+
+    private $controller, $action, $id, $data;
+
+    public function __construct(){
+
+        $this->controller = $_GET["controller"] ?? "";
+        $this->action = $_GET["action"] ?? APP_DEFAULT_ACTION;
+        $this->id = $_GET["id"] ?? 0;
+        $this->data = json_decode(file_get_contents("php://input"), true);
+    }
+
+    public function getController(): string{
+        return $this->controller;
+    }
+    public function getAction(): string{
+        return $this->action;
+    }
+    public function getId(): int{
+        return $this->id;
+    }
+    public function getData(): array{
+        if ($this->data == null) {
+            throw new \Exception("ACCION INVALIDA!");
+        } else {
+            return $this->data;
+        }
+    }
+    public function getRequestMethod(): string{
+        return $_SERVER["REQUEST_METHOD"];
+    }
+    public function setController($controller): void{
+        $this->controller = $controller;
+    }
+    public function setAction($action): void{
+        $this->action = $action;
+    }
+}
