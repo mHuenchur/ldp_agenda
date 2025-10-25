c80beecf6b2c13b9504436c89e269bc2a7f370e4
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-25 08:37:54 -0300

    Template base para views

---
+++ app/resources/template/template.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php
+    require_once APP_TEMPLATE . "includes/head.php";
+    
+    foreach($this->scripts as $script){
+        echo ('<script defer type="text/javascript" src=" '. $script .' "></script>');
+    }
+
+    ?>
+</head>
+<body>
+    <header>
+    <?php
+    
+    if(isset($_SESSION["token"]) && $_SESSION["token"] === APP_TOKEN){
+        if ($_SESSION["perfil"] === "usuario") {
+            require_once APP_TEMPLATE . "includes/usuarioHeader.php";
+        } else {
+            if ($_SESSION["perfil"] === "administrador") {
+                require_once APP_TEMPLATE . "includes/administradorHeader.php";
+            }
+        }
+    }/*else {
+        if ($this->view != "autenticacion/logout.php") {
+            require_once APP_TEMPLATE . "includes/autenticacionHeader.php";
+        }
+    }*/
+
+    ?>
+    </header>
+    <main>
+    <?php
+    require_once APP_VIEWS . $this->view;
+    ?>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
