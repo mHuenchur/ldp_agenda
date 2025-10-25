f2c9218825535869a3d9d8a8f4b230d24d295d4e
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-25 08:21:43 -0300

    Configuracion de la aplicacion y base de datos

--- app/config/AppConfig.php
+++ app/config/AppConfig.php
@@ -1,11 +1,16 @@
 <?php
 
-define("APP_URL","");
+define("APP_URL","http://localhost/ldp_agenda/");
 define("APP_URI",$_SERVER["DOCUMENT_ROOT"]."/ldp_agenda/app/");
+
+define("APP_TEMPLATE", APP_URI . "resources/template/");
+define("APP_VIEWS", APP_URI . "resources/views/");
+
+define("APP_MODELS", APP_URI . "core/model/");
+define("APP_SERVICES", APP_URI . "core/service/");
 
 CONST APP_TOKEN = "SECRET_KEY";
 
 define("APP_DEFAULT_ACTION", "index");
-
 
 ?>
--- app/config/DBConfig.php
+++ app/config/DBConfig.php
@@ -1,6 +1,6 @@
 <?php
 
-CONST DB_DSN = "";
+CONST DB_DSN = "mysql:host=localhost;dbname=ldp_agenda";
 CONST DB_USER = "root";
 CONST DB_PASS = "";
 
