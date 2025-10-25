1eebb42902198c218dd759aa38b49f0eea8c0379
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-25 08:11:20 -0300

    Archivos DAO y estructura base

---
+++ app/core/model/dao/RecordatorioDAO.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\core\model\dao;
+
+use app\core\model\base\DAO;
+use app\core\model\base\InterfaceDAO;
+use app\core\model\base\InterfaceDTO;
+
+final class RecordatorioDAO extends DAO implements InterfaceDAO
+{
+    public function __construct($conn)
+    {
+        parent::__construct($conn, "recordatorio");
+    }
+
+    public function save(InterfaceDTO $object): void
+    {
+
+    }
+
+    public function load($id): InterfaceDTO
+    {
+        return  new InterfaceDTO();
+    }
+
+    public function update(InterfaceDTO $object): void
+    {
+        
+    }
+
+    public function delete($id): void
+    {
+        
+    }
+
+    public function list(): array
+    {
+        return array();
+    }
+
+}
+
+
+?>
---
+++ app/core/model/dao/UsuarioDAO.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\core\model\dao;
+
+use app\core\model\base\DAO;
+use app\core\model\base\InterfaceDAO;
+use app\core\model\base\InterfaceDTO;
+
+final class UsuarioDAO extends DAO implements InterfaceDAO
+{
+    public function __construct($conn)
+    {
+        parent::__construct($conn, "usuario");
+    }
+
+    public function save(InterfaceDTO $object): void
+    {
+        $sql = "INSERT INTO {$this->table} VALUES (DEFAULT, :nombre, :apellido, :nombreUsuario, :correo, :clave)";
+        $stmt = $this->conn->prepare($sql);
+        $data = $object->toArray();
+        unset($data["id"]);
+        unset($data["perfilID"]);
+
+
+        $clave = password_hash($object->getClave(), PASSWORD_DEFAULT);
+        $data["clave"] = $clave;
+
+
+        $stmt->execute($data);
+    }
+
+    public function load($id): InterfaceDTO
+    {
+        return  new InterfaceDTO();
+    }
+
+    public function update(InterfaceDTO $object): void
+    {
+        
+    }
+
+    public function delete($id): void
+    {
+        
+    }
+
+    public function list(): array
+    {
+        return array();
+    }
+
+}
+
+
+?>
