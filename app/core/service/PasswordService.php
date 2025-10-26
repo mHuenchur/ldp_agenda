97a90fbee10e70bf838fa8cc714c3439d4190934
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-26 14:54:02 -0300

    service back para password

---
+++ app/core/service/PasswordService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\core\service;
+
+use app\core\model\base\InterfaceDTO;
+use app\core\model\dao\TokenDAO;
+use app\core\service\base\Service;
+use app\core\service\base\InterfaceService;
+use app\libs\connection\Connection;
+use app\core\model\dao\UsuarioDAO;
+use app\core\model\dto\UsuarioDTO;
+
+
+final class PasswordService extends Service implements InterfaceService{
+    
+    public function save(array $object): void{
+
+    }
+
+    public function load($id): InterfaceDTO{
+        return new InterfaceDTO;
+    }
+
+    public function update(array $object): void{
+
+    }
+
+    public function delete($id): void{
+
+    }
+
+    public function list(): array{
+        return [];
+    }
+    
+    public function emailCheck($email): string{
+        $conn = Connection::get();
+        $dao = new UsuarioDAO($conn);
+        return $dao->emailCheck($email);
+    }
+    public function generateToken($id, $email): string{
+        $conn = Connection::get();
+        $token = bin2hex(random_bytes(32));
+        $dao = new TokenDAO($conn);
+        $dao->create($id, $email, $token);
+        return $token;
+    }
+}
