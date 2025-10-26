b29c258e278a17e6a8fd43c9c6dfcf5e221b78f0
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-10-26 14:51:13 -0300

    Metodo passwordLost

--- app/core/controller/AutenticacionController.php
+++ app/core/controller/AutenticacionController.php
@@ -2,15 +2,14 @@
 
 namespace app\core\controller;
 
-use app\core\controller\base\InterfaceController;
+
 use app\core\controller\base\Controller;
-use app\core\service\CarteleraVigenteService;
-use app\core\service\FuncionService;
 use app\libs\request\Request;
 use app\libs\response\Response;
-use app\libs\autentication\Authentication;
+use app\libs\authentication\Authentication;
 use app\core\service\UsuarioService;
-use app\libs\mailManager\MailManager;
+use app\core\service\PasswordService;
+use app\libs\mail\MailManager;
 
 final class AutenticacionController extends Controller{
 
@@ -49,5 +48,34 @@ final class AutenticacionController extends Controller{
         $response->send();
     }
 
+    public function passwordLost(): void{
+        $this->view = "autenticacion/changePassword.php";
+        require_once APP_TEMPLATE . "template.php";
+    }
 
+    public function reset(Request $request, Response $response): void{
+        $service = new PasswordService();
+        $email = $request->getData();
+        //verifica si el mail existe
+        $valor = $service->emailCheck($email["correo"]);
+        if ($valor !== NULL) {
+            //genera token en bd
+            $token = $service->generateToken($valor, $email["correo"]);
+            //envia correo
+            MailManager::createMail($email["correo"], $token);
+            //ENVIAR MENSAJE GENERICO, MODIFICAR DESPUES A UN SOLO MESSAGE!
+            $response->setMessage("Si todo esta bien se envia un correo!");
+            
+        } else {
+            $response->setMessage("Dato incorrecto!");
+        }
+        $response->send();
+    }
+
+    public function resetPage(Request $request, Response $response): void{
+        //verificamos que ese url aun este valido
+        //si esta valido deberia permitir utilizar 
+        $this->view = "autenticacion/resetPage.php";
+        require_once APP_TEMPLATE . "template.php";
+    }
 }
