34dcf85991749c45cdb0a97d4e11b0498db0d819
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-01 20:42:02 -0300

    Controllers categoria, contacto y recordatorio

---
+++ app/core/controller/CategoriaController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\core\controller;
+
+use app\core\controller\base\InterfaceController;
+use app\core\controller\base\Controller;
+use app\libs\response\Response;
+use app\libs\request\Request;
+
+final class CategoriaController extends Controller implements InterfaceController{
+
+    public function __construct()
+    {
+        parent::__construct([
+            
+        ]);
+    }
+    // BUSCA EL INICIO DE LA VISTA CORRESPONDIENTE
+    public function index(Request $request, Response $response): void{
+        $this->view = "categoria/index.php";
+        require_once APP_TEMPLATE . "template.php";
+    }
+    // BUSCA UN ELEMENTO EN PARTICULAR
+    public function load(Request $request, Response $response): void{
+
+    }
+    // BUSCA LA VISTA DE CREAR UNA NUEVA ENTIDAD
+    public function create(Request $request, Response $response): void{
+
+    }
+    // GESTIONA EL GUARDADO DE UNA NUEVA ENTIDAD EN EL SISTEMA
+    public function save(Request $request, Response $response): void{
+
+    }
+    // BUSCA LA VISTA DE EDITAR UNA ENTIDAD EXISTENTE EN EL SISTEMA
+    public function edit(Request $request, Response $response): void{
+
+    }
+    // BUSCA LA VISTA DE EDITAR UNA ENTIDAD EXISTENTE EN EL SISTEMA
+    public function update(Request $request, Response $response): void{
+
+    }
+    //GESTIONA LA ELIMINACION DE UNA ENTIDAD DEL SISTEMA
+    public function delete(Request $request, Response $response): void{
+        
+    }
+
+}
