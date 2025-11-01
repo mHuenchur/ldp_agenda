fe15d92ffe2f67dea3a5f179540b1af6964fa647
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-01 20:37:16 -0300

    Modificacion header usuario

--- app/resources/template/includes/usuarioHeader.php
+++ app/resources/template/includes/usuarioHeader.php
@@ -1,29 +1,36 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary-subtle">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary-subtle fixed-top">
 <div class="container-fluid">
-    <a class="navbar-brand" href="usuario/index">
-    <img src="public/assets/gallery/home.png" alt="Bootstrap" width="30" height="24">
+    <a class="navbar-brand px-2" href="usuario/index">
+    <img src="public/assets/gallery/home_1.png" alt="Bootstrap" width="30" height="24">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <li class="nav-item px-4">
+                <a class="nav-link active d-flex flex-column align-items-center py-2" href="contacto/index" aria-current="page">
+                <img class="nav-icon mb-1" src="public/assets/gallery/people.png" alt="" role="presentation">
+                <small>Contactos</small>
+                </a>
             </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <li class="nav-item px-4">
+                <a class="nav-link active d-flex flex-column align-items-center py-2" href="recordatorio/index" aria-current="page">
+                <img class="nav-icon mb-1" src="public/assets/gallery/bell.png" alt="" role="presentation">
+                <small>Recordatorios</small>
+                </a>
             </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <li class="nav-item px-4">
+                <a class="nav-link active d-flex flex-column align-items-center py-2" href="categoria/index" aria-current="page">
+                <img class="nav-icon mb-1" src="public/assets/gallery/list.png" alt="" role="presentation">
+                <small>Categorías</small>
+                </a>
+            </li>
+            <li class="nav-item px-4">
+                <a class="nav-link active d-flex flex-column align-items-center py-2" href="usuario/perfil" aria-current="page">
+                <img class="nav-icon mb-1" src="public/assets/gallery/user.png" alt="" role="presentation">
+                <small>Perfil</small>
+                </a>
             </li>
         </ul>
         <div class="nav-item d-flex justify-content-end">
@@ -31,4 +38,4 @@
         </div>
     </div>
 </div>
-</nav>
+</nav> 
