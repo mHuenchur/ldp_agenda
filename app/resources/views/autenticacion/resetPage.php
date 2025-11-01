4fc008ac5faec73c8475dda9c89d24db93d88cd0
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-01 20:35:50 -0300

    Vista para cambiar la clave

--- app/resources/views/autenticacion/resetPage.php
+++ app/resources/views/autenticacion/resetPage.php
@@ -1,1 +1,21 @@
-<h5>Hola soy el page reset</h5>
+
+<div class="card w-25 mb-3 mx-auto mt-5">
+    <div class="card-header">
+        Cambio de clave
+    </div>
+    <div class="card-body">
+        <div class="">
+            <input type="hidden" name="key" id="key" value="<?php echo $token ?>">
+            <label for="claveNueva" class="form-label">Clave</label>
+            <input type="password" class="form-control" id="claveNueva" aria-describedby="claveHelpBlock" required>
+            <div id="claveHelpBlock" class="form-text mt-2">
+                Ingresa tu nueva clave.
+            </div>
+        </div>
+    </div>
+    <div class="card-footer text-body-secondary">
+        <div>
+            <button type="button" class="btn btn-primary" onclick="autenticacionController.resetPassword()">Confirmar</button>
+        </div>
+    </div>
+</div>
