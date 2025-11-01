456fab7173d45fc8644c892a96f745454c82db58
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-01 20:37:40 -0300

    Agrega url custom.css

--- app/resources/template/includes/head.php
+++ app/resources/template/includes/head.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <base href="http://localhost/ldp_agenda/">
+<link rel="stylesheet" href="public/assets/css/custom.css">
 <link rel="stylesheet" href="public/assets/libs/bootstrap-5.3.8-dist/css/bootstrap.min.css">
 <script src="public/assets/libs/bootstrap-5.3.8-dist/js/bootstrap.min.js"></script>
 <title>AGENDA CONTACTOS</title>
