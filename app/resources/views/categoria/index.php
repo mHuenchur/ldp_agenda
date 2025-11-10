741b8d2b921ab1611bbbc9924adc300c516b2e58
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-10 17:00:14 -0300

    Modal agregar nueva categoria

--- app/resources/views/categoria/index.php
+++ app/resources/views/categoria/index.php
@@ -6,7 +6,7 @@
       <div class="card-body d-flex flex-wrap justify-content-between align-items-center gap-2">
         <h5 class="mb-0">Operaciones</h5>
         <div class="d-flex gap-2">
-          <button class="btn btn-primary btn-sm">Nueva operación</button>
+          <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#categoriaModal">Nueva categoria</button>
           <button class="btn btn-outline-secondary btn-sm">Exportar</button>
         </div>
       </div>
@@ -30,5 +30,31 @@
         </li>
       </ul>
     </section>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="categoriaModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="modalLabel">Nueva categoria</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form id="formNuevaCategoria" action="">
+            <div class="mb-3">
+              <label for="inputNombreCategoria" class="form-label">Nombre</label>
+              <input type="text" class="form-control" id="inputNombreCategoria" required minlength="3" maxlength="45">
+
+            </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button id="btnNuevaCategoria" type="button" class="btn btn-primary">Guardar</button>
+          </div>
+        </form>
+
+      </div>
   </div>
 </div>
