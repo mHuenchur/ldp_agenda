2fc8d81877d0ffbe80fe8fe6bc4d951cce47902c
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-10 17:07:01 -0300

    Archivo categoria service

---
+++ app/core/service/CategoriaService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\core\service;
+
+use app\core\model\base\InterfaceDTO;
+use app\core\model\dao\CategoriaDAO;
+use app\core\model\dto\CategoriaDTO;
+use app\core\service\base\Service;
+use app\core\service\base\InterfaceService;
+use app\libs\connection\Connection;
+
+
+
+final class CategoriaService extends Service implements InterfaceService{
+    
+    public function save(array $object): void{
+        $conn = Connection::get();
+        $dao = new CategoriaDAO($conn);
+        $dao->save(new CategoriaDTO($object));
+    }
+
+    public function load($id): InterfaceDTO{
+        return new InterfaceDTO;
+    }
+
+    public function update(array $object): void{
+
+    }
+
+    public function delete($id): void{
+
+    }
+
+    public function list(): array{
+        return [];
+    }
+    
+}
