86d418de388aff78eea9cc340d19e59457f68e48
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-30 18:47:23 -0300

    Modificacion metodo save, implementacion metodos load, update, updateClave UsuarioDAO

--- app/core/model/dao/UsuarioDAO.php
+++ app/core/model/dao/UsuarioDAO.php
@@ -5,6 +5,7 @@ namespace app\core\model\dao;
 use app\core\model\base\DAO;
 use app\core\model\base\InterfaceDAO;
 use app\core\model\base\InterfaceDTO;
+use app\core\model\dto\UsuarioDTO;
 
 final class UsuarioDAO extends DAO implements InterfaceDAO
 {
@@ -13,13 +14,13 @@ final class UsuarioDAO extends DAO implements InterfaceDAO
         parent::__construct($conn, "usuario");
     }
 
-    public function save(InterfaceDTO $object): void
+    public function save(InterfaceDTO $object)
     {
-        $sql = "INSERT INTO {$this->table} VALUES (DEFAULT, :nombre, :apellido, :nombreUsuario, :correo, :clave)";
+        $sql = "INSERT INTO {$this->table} VALUES (DEFAULT, :nombre, :apellido, :nombre_usuario, :email, :clave, :tiempo_notificacion)";
         $stmt = $this->conn->prepare($sql);
         $data = $object->toArray();
         unset($data["id"]);
-        unset($data["perfilID"]);
+        unset($data["perfil_id"]);
 
 
         $clave = password_hash($object->getClave(), PASSWORD_DEFAULT);
@@ -27,16 +28,51 @@ final class UsuarioDAO extends DAO implements InterfaceDAO
 
 
         $stmt->execute($data);
+        return $this->conn->lastInsertId();
     }
 
     public function load($id): InterfaceDTO
     {
-        return  new InterfaceDTO();
+        $sql = "SELECT `id`, `nombre`, `apellido`, `nombre_usuario` AS 'nombreUsuario', `email` AS 'correo', `tiempo_notificacion` AS 'tiempo' FROM `usuario` WHERE usuario.id = :uid";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            'uid' => $_SESSION["id"],
+        ]);
+        return new UsuarioDTO($stmt->fetch(\PDO::FETCH_ASSOC));
     }
 
     public function update(InterfaceDTO $object): void
     {
+        $sql = "UPDATE `usuario` SET `nombre`= :nombre,`apellido`= :apellido,`nombre_usuario`= :nombre_usuario,`email`= :email,`tiempo_notificacion`= :tiempo_notificacion 
+        WHERE usuario.id = ". $_SESSION["id"];
+        $stmt = $this->conn->prepare($sql);
+        $data = $object->toArray();
+        unset($data["id"]);
+        unset($data["clave"]);
+        unset($data["perfil_id"]);
+        $stmt->execute($data);
+    }
+    public function updateClave($object)
+    {
+        $sql = "SELECT clave FROM `usuario` 
+        WHERE usuario.id = ".$_SESSION["id"];
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $cuenta = $stmt->fetch(\PDO::FETCH_OBJ);
         
+        if(!password_verify($object["clave"], $cuenta->clave)){
+            throw new \Exception("clave es inválido");
+        }else {
+            $clave = password_hash($object["nueva"], PASSWORD_DEFAULT);
+
+            $sql = "UPDATE `usuario` SET `clave`= :clave
+            WHERE usuario.id = ". $_SESSION["id"];
+            
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                "clave" => $clave
+            ]);
+        }
     }
 
     public function delete($id): void
