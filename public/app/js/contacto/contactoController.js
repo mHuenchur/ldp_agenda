d1d0ae5a3b5993ae07cd5380a21482e512c42c83
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-30 22:25:45 -0300

    Implementacion js de guardar y filtrar contactoController

--- public/app/js/contacto/contactoController.js
+++ public/app/js/contacto/contactoController.js
@@ -1,20 +1,19 @@
 let contactoController = {
     dataContacto: {
-        id: "101",
-        nombre: "Max",
-        apellido: "Power",
-        razon_social: "debe llegar a db vacio",
-        direccion: "Sarmiento 2500",
-        email: "PowerM@gmail.com",
-        sitio_web: "TheMaxPower",
+        id: "",
+        nombre: "",
+        apellido: "",
+        razon_social: "",
+        direccion: "",
+        email: "",
+        sitio_web: "",
         fecha_nacimiento: "",
-        observaciones: "Awesome guy!",
-        tipo: "1",
-        categoria: "2",
+        observaciones: "",
+        tipo: "",
+        categoria: "",
         usuario: "",
         telefonos: [
-            {etiqueta: "móvil", numero: "444555"},
-            {etiqueta: "trabajo", numero: "666777"}
+
         ]
     },
     dataFiltros: {
@@ -23,17 +22,88 @@ let contactoController = {
     },
     saveContacto: () => {
         //VALIDAR CAMPOS
-        //ENVIAR AL SERVICE
-        contactoService.saveContacto(contactoController.dataContacto)
-        .then(response => {
+        if (contactoController.validacion()) {
+            const list = document.getElementById('telList');
+            contactoController.dataContacto.nombre = document.getElementById("nombre").value;
+            contactoController.dataContacto.apellido = document.getElementById("apellido").value;
+            contactoController.dataContacto.fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
+            contactoController.dataContacto.razon_social = document.getElementById("razon_social").value;
+            contactoController.dataContacto.direccion = document.getElementById("direccion").value;
+            contactoController.dataContacto.sitio_web = document.getElementById("sitio_web").value;
+            contactoController.dataContacto.categoria = document.getElementById("categoria").value;
+            contactoController.dataContacto.tipo = document.getElementById("tipo").value;
+            contactoController.dataContacto.email = document.getElementById("email").value;
+            contactoController.dataContacto.observaciones = document.getElementById("observaciones").value;
+
+            for (const fila of list.querySelectorAll('.fila')) {
+                const etiqueta = fila.querySelector('[name="etiqueta"]').value.trim();
+                const numero = fila.querySelector('[name="numero"]').value.trim();
+                const contacto_id = "";
+                contactoController.dataContacto.telefonos.push({ etiqueta, numero, contacto_id });
+            }
+
+            contactoService.saveContacto(contactoController.dataContacto)
+            .then(response => {
                 if(response.error === ""){
-                    console.log("GUARDADO");
+                    console.log(response.mensaje);
                 }else{
                     console.log("NO SE PUDO GUARDAR");
                 }
             })
+        } else {
+            
+        }
+        
+    },
+    validacion: () => {
+        return true;
     },
     filtrarLista: () => {
-        contactoService.filtrarLista(contactoController.dataFiltros);
+        contactoController.dataFiltros.categoria = document.getElementById("fCategoria").value;
+        contactoController.dataFiltros.tipo = document.getElementById("fTipo").value;
+        contactoService.filtrarLista(contactoController.dataFiltros)
+        .then(response => {
+                if(response.error === ""){
+                    if (!(response.result == "")) {
+                        dataTable?.destroy();
+
+                        const tabla = document.querySelector('#myTable');
+                        const tbody = tabla.tBodies[0];           // o: tabla.querySelector('tbody')
+                        tbody.innerHTML = response.result;
+
+                        dataTable = new simpleDatatables.DataTable("#myTable", {
+                            labels: {
+                            placeholder: "Buscar…",
+                            perPage: "contactos por página",              // “entries per page”
+                            noRows: "No hay registros",
+                            info: "Mostrando {start} a {end} de {rows}", // “Showing 1 to 3 of 3 entries”
+                            noResults: "No se encontraron resultados",
+                            },
+                            searchable: true,
+                            fixedHeight: true
+                        })
+                    } else {
+                        dataTable?.destroy();
+
+                        const tabla = document.querySelector('#myTable');
+                        const tbody = tabla.tBodies[0];           // o: tabla.querySelector('tbody')
+                        tbody.innerHTML = response.result;
+
+                        dataTable = new simpleDatatables.DataTable("#myTable", {
+                            labels: {
+                            placeholder: "Buscar…",
+                            perPage: "contactos por página",              // “entries per page”
+                            noRows: "No hay registros",
+                            info: "Mostrando {start} a {end} de {rows}", // “Showing 1 to 3 of 3 entries”
+                            noResults: "No se encontraron resultados",
+                            },
+                            searchable: true,
+                            fixedHeight: true
+                        })
+                    }
+                }else{
+                    console.log("NO");
+                }
+            })
     }
 }
