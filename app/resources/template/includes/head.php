20cdff4ff27a51fc7f0f8b0fd452ccf3f6cb285e
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-30 22:06:41 -0300

    URLs de simpledatatables

--- app/resources/template/includes/head.php
+++ app/resources/template/includes/head.php
@@ -4,5 +4,7 @@
 <link rel="stylesheet" href="public/assets/css/custom.css">
 <link rel="stylesheet" href="public/assets/libs/bootstrap-5.3.8-dist/css/bootstrap.min.css">
 <script src="public/assets/libs/bootstrap-5.3.8-dist/js/bootstrap.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" type="text/javascript"></script>
 <title>AGENDA CONTACTOS</title>
 <link rel="icon" href="public/assets/gallery/calendar.png" type="image/png">
