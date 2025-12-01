70106fc4448ac67207e2b8d22399ba38adfa969b
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-11-30 21:51:47 -0300

    Implementacion y modificacion de metodos save, load, update y updateClave de UsuarioService

--- app/core/service/UsuarioService.php
+++ app/core/service/UsuarioService.php
@@ -12,18 +12,28 @@ use app\core\model\dto\UsuarioDTO;
 
 final class UsuarioService extends Service implements InterfaceService{
     
-    public function save(array $object): void{
+    public function save(array $object){
         $conn = Connection::get();
         $dao = new UsuarioDAO($conn);
-        $dao->save(new UsuarioDTO($object));
+        return $dao->save(new UsuarioDTO($object));
     }
 
     public function load($id): InterfaceDTO{
-        return new InterfaceDTO;
+        $conn = Connection::get();
+        $dao = new UsuarioDAO($conn);
+        return $dao->load($id);
     }
 
     public function update(array $object): void{
+        $conn = Connection::get();
+        $dao = new UsuarioDAO($conn);
+        $dao->update(new UsuarioDTO($object));
+    }
 
+    public function updateClave(array $object): void{
+        $conn = Connection::get();
+        $dao = new UsuarioDAO($conn);
+        $dao->updateClave($object);
     }
 
     public function delete($id): void{
