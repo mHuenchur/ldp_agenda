2a5ef6ce05d903c55d0a8458738106b9e459f0a3
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-12-02 19:30:13 -0300

    Implementacion updateRecordatorio de JS service

--- public/app/js/recordatorio/recordatorioService.js
+++ public/app/js/recordatorio/recordatorioService.js
@@ -17,5 +17,24 @@ let recordatorioService = {
         .catch(error => {
             console.error("ERROR EN LA PETICION ", error)
         });
+    },
+    updateRecordatorio: (data) => {
+        return fetch("recordatorio/update", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.error("ERROR EN LA PETICION ", error)
+        });
     }
 }
