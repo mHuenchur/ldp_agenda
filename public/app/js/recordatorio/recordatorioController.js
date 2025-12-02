8c49098cdadca53087c7b63615844fe8a386ae60
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-12-02 19:29:43 -0300

    Implementacion updateRecordatorio JS de recordatorio

--- public/app/js/recordatorio/recordatorioController.js
+++ public/app/js/recordatorio/recordatorioController.js
@@ -32,6 +32,30 @@ let recordatorioController = {
             })
         }
     },
+    updateRecordatorio: () => {
+        if (recordatorioController.validacion()) {
+            const select = document.getElementById('contactos');
+            recordatorioController.dataRecordatorio.id = document.getElementById("id").value;
+            recordatorioController.dataRecordatorio.nombre = document.getElementById("descripcion").value;
+            recordatorioController.dataRecordatorio.fecha_hora = document.getElementById("fecha_hora").value;
+            recordatorioController.dataRecordatorio.lugar = document.getElementById("lugar").value;
+
+            [...select.selectedOptions].forEach(o => {
+            const id = Number(o.value);
+            if (Number.isFinite(id)) {
+            recordatorioController.dataRecordatorio.contactos.push({ contacto: id });
+            }
+            });
+            recordatorioService.updateRecordatorio(recordatorioController.dataRecordatorio)
+            .then(response => {
+                if(response.error === ""){
+                    console.log(response.mensaje);
+                }else{
+                    console.log("NO SE PUDO GUARDAR");
+                }
+            })
+        }
+    },
     validacion: () => {
         return true;
     }
