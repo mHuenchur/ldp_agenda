162f48ceea4b46c8ee3a94decd87c5c639187c61
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-12-08 15:59:00 -0300

    Excepciones para save y update contacto service

--- app/core/service/ContactoService.php
+++ app/core/service/ContactoService.php
@@ -4,7 +4,9 @@ namespace app\core\service;
 
 use app\core\model\base\InterfaceDTO;
 use app\core\model\dao\ContactoDAO;
+use app\core\model\dao\TelefonoDAO;
 use app\core\model\dto\ContactoDTO;
+use app\core\model\dto\TelefonoDTO;
 use app\core\service\base\Service;
 use app\core\service\base\InterfaceService;
 use app\libs\connection\Connection;
@@ -15,9 +17,62 @@ final class ContactoService extends Service implements InterfaceService{
     
     public function save(array $object){
         $conn = Connection::get();
-        $dao = new ContactoDAO($conn);
-        return $dao->save(new ContactoDTO($object));
-        //FALTA DAO TELEFONO CON EL ID ETIQUETA Y EL ID DEL CONTACTO QUE AGREGAMOS
+        $daoContacto = new ContactoDAO($conn);
+        $daoTelefono = new TelefonoDAO($conn);
+
+        $telefonos = $object["telefonos"];
+        $telefonosVerificados = [];
+
+        $contacto = new ContactoDTO($object);
+        //validaciones
+        if ($contacto->getIdTipo() == "") {
+            throw new \Exception("Campo tipo vacio.");
+        }
+        if ($contacto->getIdTipo() == "1" && $contacto->getNombre() == '') {
+            throw new \Exception("Campo nombre vacio.");
+        }
+        if ($contacto->getIdTipo() == "1" && $contacto->getApellido() == '') {
+            throw new \Exception("Campo apellido vacio.");
+        }
+        if ($contacto->getIdTipo() == "1" && $contacto->getFechaNacimiento() == '') {
+            throw new \Exception("Campo fecha nacimiento vacio.");
+        }
+        if ($contacto->getIdTipo() == "2" && $contacto->getRazonSocial() == '') {
+            throw new \Exception("Campo razon social vacio.");
+        }
+        if ($contacto->getDireccion() == '') {
+            throw new \Exception("Campo dirección vacio.");
+        }
+        if ($contacto->getEmail() == '' || (!filter_var($contacto->getEmail(), FILTER_VALIDATE_EMAIL))) {
+            throw new \Exception("Campo correo vacio o formato incorrecto.");
+        }
+        if ($contacto->getSitioWeb() == '') {
+            throw new \Exception("Campo sitio web vacio.");
+        }
+        if ($contacto->getObservaciones() == '') {
+            throw new \Exception("Campo observaciones vacio.");
+        }
+        if ($contacto->getIdCategoria() == '') {
+            throw new \Exception("Campo categoria vacio.");
+        }
+
+        foreach ($telefonos as $telefono) {
+            $itemTelefono = new TelefonoDTO($telefono);
+            if ($itemTelefono->getEtiqueta() == '') {
+                throw new \Exception("Existen etiquetas vacias.");
+            }
+            if ($itemTelefono->getNumero() == '') {
+                throw new \Exception("Existen numeros vacios.");
+            }
+            array_push($telefonosVerificados, $itemTelefono);
+        }
+
+
+        $valorContacto =  $daoContacto->save($contacto);
+        foreach ($telefonosVerificados as $telefonoVerificado) {
+            $telefonoVerificado->setContactoId($valorContacto);
+            $daoTelefono->save($telefonoVerificado);
+        }
     }
 
     public function load($id): InterfaceDTO{
@@ -29,7 +84,42 @@ final class ContactoService extends Service implements InterfaceService{
     public function update(array $object): void{
         $conn = Connection::get();
         $dao = new ContactoDAO($conn);
-        $dao->update(new ContactoDTO($object));
+        $contacto = new ContactoDTO($object);
+
+
+        //validaciones
+        if ($contacto->getIdTipo() == "") {
+            throw new \Exception("Campo tipo vacio.");
+        }
+        if ($contacto->getIdTipo() == "1" && $contacto->getNombre() == '') {
+            throw new \Exception("Campo nombre vacio.");
+        }
+        if ($contacto->getIdTipo() == "1" && $contacto->getApellido() == '') {
+            throw new \Exception("Campo apellido vacio.");
+        }
+        if ($contacto->getIdTipo() == "1" && $contacto->getFechaNacimiento() == '') {
+            throw new \Exception("Campo fecha nacimiento vacio.");
+        }
+        if ($contacto->getIdTipo() == "2" && $contacto->getRazonSocial() == '') {
+            throw new \Exception("Campo razon social vacio.");
+        }
+        if ($contacto->getDireccion() == '') {
+            throw new \Exception("Campo dirección vacio.");
+        }
+        if ($contacto->getEmail() == '' || (!filter_var($contacto->getEmail(), FILTER_VALIDATE_EMAIL))) {
+            throw new \Exception("Campo correo vacio o formato incorrecto.");
+        }
+        if ($contacto->getSitioWeb() == '') {
+            throw new \Exception("Campo sitio web vacio.");
+        }
+        if ($contacto->getObservaciones() == '') {
+            throw new \Exception("Campo observaciones vacio.");
+        }
+        if ($contacto->getIdCategoria() == '') {
+            throw new \Exception("Campo categoria vacio.");
+        }
+
+        $dao->update($contacto);
     }
 
     public function delete($id): void{
