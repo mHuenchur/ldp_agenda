989809365c6491981026571c4b30ce3dc422fce5
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-12-08 16:06:58 -0300

    Toast para vista create contacto

--- app/resources/views/contacto/create.php
+++ app/resources/views/contacto/create.php
@@ -107,3 +107,15 @@
     <button type="button" onclick=contactoController.saveContacto() class="btn btn-primary">Guardar</button>
   </div>
 </form>
+
+
+<div class="toast-container p-3 position-fixed top-0 start-50 translate-middle-x">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <strong class="me-auto">ATENCION</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div id="messageContainer" class="toast-body">
+    </div>
+  </div>
+</div>
